53f4b1c598e23c4cf8c363a3915f13aa0c0a1992
Георгий Князев <saint_georgy@yahoo.com>
2023-07-08 07:49:23 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string,string> words = new Dictionary<string,string>();
+            words.Add("Мама","не Папа");
+            words.Add("Папа","не Мама");
+
+            string userInPut;
+
+            Console.Write("Введите слово: ");
+            userInPut = Convert.ToString(Console.ReadLine());
+            
+            if (words.ContainsKey(userInPut))
+            {
+                Console.WriteLine(words[userInPut]);
+            }
+            else
+            {
+                Console.WriteLine("Такого слова нет");
+            }
+            Console.ReadKey();
+        }
+    }
+}
